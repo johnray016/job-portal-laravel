170ef43037298203de04cc701713822c4e3c070e
John Ray Pantaleon <pantaleonjohnray@gmail.com>
2018-03-07 14:59:15 +0800

    update

--- database/migrations/2018_03_07_065806_add_jobs_id_in_applicants.php
+++ database/migrations/2018_03_07_065806_add_jobs_id_in_applicants.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddForeignKeyToApplicantsTable extends Migration
+class AddJobsIdInApplicants extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,9 @@ class AddForeignKeyToApplicantsTable extends Migration
      */
     public function up()
     {
-       Schema::table('applicants', function (Blueprint $table) {
+        Schema::table('applicants', function (Blueprint $table) {
             $table->integer('job_id')->unsigned()->nullable();
+
             $table->foreign('job_id')->references('id')->on('jobs');
         });
     }

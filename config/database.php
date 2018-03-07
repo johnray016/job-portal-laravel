421a20d26534aa442f1c94c47a1962e8e5504be3
John Ray Pantaleon <pantaleonjohnray@gmail.com>
2018-03-07 10:10:30 +0800

    update database

--- config/database.php
+++ config/database.php
@@ -43,7 +43,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'iconnect'),
+            'database' => env('DB_DATABASE', 'iconnectglobal'),
             'username' => env('DB_USERNAME', 'johnray'),
             'password' => env('DB_PASSWORD', '123456'),
             'unix_socket' => env('DB_SOCKET', ''),
